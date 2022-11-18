da455c6bf926f794d977098bc6ef18086004de80
Ivan Monge <ivanmongebadilla@gmail.com>
2022-11-18 14:32:30 -0700

    Other files

--- javascript/sc.js
+++ javascript/sc.js
@@ -6,3 +6,17 @@ $(function(){
         $("#vacationModal").modal("show")
     });
 });
+
+const styPre = document.getElementById('safetyPre');
+const gwLink = document.getElementById('gowanlink');
+
+styPre.addEventListener('click', openPre);
+gwLink.addEventListener('click', openLink);
+
+function openPre(){
+    window.open("others/11-16-2022.pptx");
+}
+
+function openLink(){
+    window.open("https://kyma.com/news/home-grown/2022/11/01/gowan-company-celebrates-60th-anniversary/", "_blank");
+}
